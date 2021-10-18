ed1102449c790a4a10f03b412aabe3c995e4b2fc
rohanisitohang <rohanisitohang15@gmail.com>
2021-10-18 16:41:12 +0700

    rohanisitohang

--- challenge4.java
+++ challenge4.java
@@ -1,25 +1,48 @@
-public class challenge4 {
+public class challenge4{
 
-    public static void main(String[] args) {
-        myMethod(2);
+    // method atau function
+    public static void main(String[] args){
+        Nama ("Rohani Susanti Sitohang");
+        Kampus ("UMRAH");
+        Jurusan ("Teknik Elektro");
+        Semester (7);
+        Dapatkannilai (85);
     }
 
-     //method atau function
-     static int myMethod(int angka) {
-  
-            if(angka >=0 && angka<= 25){
-                System.out.println("E");
-            } else if ( angka>= 26 && angka <= 50){
-                System.out.println("D");
-            } else if ( angka>= 51 && angka <= 75){
-                System.out.println("C");
-            } else if ( angka>= 76 && angka <= 90){
-                System.out.println("B");
-            } else if (angka>= 91 && angka<=100){
-                System.out.println("A");   
-            } else if (angka>=100) {
-                System.out.println("Invalid Number");
-            } 
-            return angka;  
-        }       
+    // Nama
+    static void Nama(String Nama){
+        System.out.println("Nama :" + Nama);
+    }
+
+    // Kampus
+    static void Kampus(String Kampus){
+        System.out.println("Kampus :" + Kampus);
+    }
+
+    // Jurusan
+    static void Jurusan(String Jurusan){
+        System.out.println( "Jurusan: " + Jurusan);
+    }
+
+// Semester
+static void Semester(int Semester){
+    System.out.println( "Semester :" + Semester);
+}
+
+    // untuk mendapatkan nilai (E= 0-20) (D= 21-40) (C= 41-60) (B= 61-80) (A= 81-100)
+    static void Dapatkannilai(int nilai){
+        if(nilai >= 0 && nilai <= 20){
+            System.out.println("nilai = E");
+        }else if(nilai >= 21 && nilai <= 40){
+            System.out.println("nilai =D");
+        }else if (nilai >= 41 && nilai <= 60){
+            System.out.println("nilai = C");
+        }else if (nilai >= 61 && nilai <= 80){
+            System.out.println("nilai = B");
+        }else if (nilai >= 81 && nilai <= 100){
+            System.out.println("nilai = A");
+        }else if (nilai >=100){
+            System.out.println("Tidak ada nilai");
+        }
+    }
 }
